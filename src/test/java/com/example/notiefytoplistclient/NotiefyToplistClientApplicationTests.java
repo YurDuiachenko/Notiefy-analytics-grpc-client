3a3a4f5dbb7ab2e67a0d8452ce0a021fd5daa6fe
YurDuiachenko <d.yurok22@mail.ru>
2024-12-12 10:54:56 +0300

    init

---
+++ src/test/java/com/example/notiefytoplistclient/NotiefyToplistClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notiefytoplistclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotiefyToplistClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
